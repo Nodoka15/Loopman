360e2c599a81d9cf278e434ddd7c12941a93d394
Nam <157942114+Nodoka15@users.noreply.github.com>
2026-07-27 16:10:14 -0400

    Gives functionality for the player to guess the entire word when they are confident. Added comments to improve code readability. Changed size of the input box to accomodate whole words.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-//Change variable is accordance to HTML
+//HTML Variables
 /*
-Theme: The Displayed Theme
-Man: How Much of Hangman Is Added
-Guessed: Characters Already Guessed
-Correct: Displayed How Much is Correct
-Input: Input from the HTML Input (Uses correctProgress)
+theme: Theme of the word displayed to the player
+hint: Hint of the word displayed to the player
+guessed: Arbitrary holder for letters the player has guessed to render the guessed box
+guessedDisplay: Using the guessed holder, it displays boxes of either red (incorrect) or green (correct) letters
+correct: Similar to guessedDisplay, displays characters of the word the user has correctly guessed
+input: The input box where player enters a letter or the whole word
+rulesOverlay: The overlay that displays the rules of the game
+infobtn: The button to click to open the rulesOverlay
+closerulesbtn: The button to close the rulesOverlay
+finishedOverlay: An overlay that opens up when the user finishes the game (either via won or lost)
+restartbtn: Button the finishedOverlay that allows the user to start up a new game
+closebtn: Button to close the finishedOverlay and complete the game
+status: Status for if the user won or lost the game
+alreadyUsed: Display whether the user has already guessed a letter
  */
-
 const theme = document.getElementById("theme");
 const hint = document.getElementById("hint");
 const guessed = document.getElementById("guessed");
@@ -22,6 +30,19 @@ const closebtn = document.getElementById("closebtn");
 const status = document.getElementById("status");
 const alreadyUsed = document.getElementById("alreadyUsed");
 
+//Other Essential Variables to Understand
+/*
+response: The response from the Word Game DB API call
+data: Data from the response variable
+wordChosen: The chosen word the player has to guess
+themeChosen: The theme of the word
+hintChosen: The hint of the word
+correctProgress: A string that mimics wordChosen but has '_' for letters not guessed yet by the player
+correctArray: wordChosen split up into an array to check individual letters and negate any spaces (null)
+ */
+
+
+//List of body parts to show as the player guesses incorrectly
 const bodyParts = [
     document.getElementById("mouth"),
     document.getElementById("eyes"),
@@ -33,7 +54,9 @@ const bodyParts = [
     document.getElementById("head"),
 ]
 
+//Function to get a new word via an API call to Word Game DB
 async function getNewWord(){
+
     document.getElementById("game-container").style.display = "none";
     document.getElementById("loader-page").style.display = "flex";
 
@@ -67,6 +90,7 @@ async function getNewWord(){
     renderWord();
 }
 
+//Function to reset the game by clearing all data
 function resetGame(){
     attemptsRemaining = 8;
     correctProgress = "";
@@ -84,10 +108,12 @@ function resetGame(){
     getNewWord();
 }
 
+//Function to count how many times char appears in str
 function count(str, char){
     return str.split(char).length - 1;
 }
 
+//Function to render letters of the word for letters that the player correctly guesses
 function renderWord(){
     correct.innerHTML = "";
     const words = correctProgress.split(" ");
@@ -107,6 +133,26 @@ function renderWord(){
     }
 }
 
+//Function to render the full word if the player correctly guesses the whole word
+function renderFullWord(){
+    correct.innerHTML = "";
+    correctProgress = wordChosen;
+    const words = correctProgress.split(" ");
+    for(let word of words){
+        const wordGroup = document.createElement("div");
+        wordGroup.className = "word-group";
+
+        for(let char of word){
+            const box = document.createElement("div");
+            box.className = "char-box";
+            box.innerText = char;
+            wordGroup.appendChild(box);
+        }
+        correct.appendChild(wordGroup);
+    }
+}
+
+//Function to render in the "guessed box" whether a guessed character is correct (green) or wrong (red)
 function renderGuessed(condition, guess){
     const guessedChar = document.createElement("div");
     guessedChar.className = condition ? "guessed-correct" : "guessed-wrong";
@@ -114,88 +160,133 @@ function renderGuessed(condition, guess){
     guessedDisplay.appendChild(guessedChar);
 }
 
+
+//Function to define what happens when the player attempts to guess the entire word
+function fullWordAttempt(guess){
+    //If the whole word guess is correct
+    if (guess === wordChosen){
+        renderFullWord();
+        status.innerText = "You Win!";
+        document.getElementById("input").disabled = true;
+        finishedOverlay.style.display = "flex";
+    }
+    //If the whole word guess is incorrect
+    else{
+        attemptsRemaining--;
+        bodyParts[attemptsRemaining].classList.remove("hidden");
+        if(attemptsRemaining === 2){
+            hint.innerText = hintChosen;
+        }
+        //Check if player ran out of tries
+        if(attemptsRemaining === 0){
+            status.innerText = "You Lose :( \nThe Correct Word is: " + wordChosen;
+            document.getElementById("input").disabled = true;
+            finishedOverlay.style.display = "flex";
+        }
+    }
+}
+
+//Function to define what happens when the player attempts to guess one letter of the word
+function letterAttempt(guess){
+    //Singular letter is wrong
+    if (guessed.innerText.indexOf(guess) !== -1){
+        alreadyUsed.innerText = "You already guessed this character!";
+        return;
+    }
+    else{
+        guessed.innerText += guess;
+        alreadyUsed.innerText = "";
+    }
+    //Find index of the character guessed
+    let rightIndex = correctArray.indexOf(guess);
+    if(rightIndex !== -1) {
+        renderGuessed(true, guess);
+        let amountRight = count(wordChosen, guess);
+        for(let i = 0; i < amountRight; i++) {
+            //Updates the right characters guessed
+            rightIndex = correctArray.indexOf(guess);
+            correctProgress = correctProgress.split('');
+            correctProgress[rightIndex] = guess;
+            correctProgress = correctProgress.join('');
+            //Deletes right character to look for remaining characters in the next search
+            delete correctArray[rightIndex];
+        }
+        //Updates the visual for the user
+        renderWord();
+        //Check if the whole word is guessed
+        if(correctProgress === wordChosen) {
+            status.innerText = "You Win!";
+            document.getElementById("input").disabled = true;
+            finishedOverlay.style.display = "flex";
+        }
+    }
+    else{
+        //Display wrong attempt
+        renderGuessed(false, guess);
+        attemptsRemaining--;
+        bodyParts[attemptsRemaining].classList.remove("hidden");
+        if(attemptsRemaining === 2){
+            hint.innerText = hintChosen;
+        }
+        //Check if ran out of tries
+        if(attemptsRemaining === 0){
+            status.innerText = "You Lose :( \nThe Correct Word is: " + wordChosen;
+            document.getElementById("input").disabled = true;
+            finishedOverlay.style.display = "flex";
+        }
+    }
+}
+
+//Event for when the user attempts to either guess one letter or the whole word
 input.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         const guess = input.value.toUpperCase();
         input.value = "";
-        //Check if a character is wrong
-        if (guessed.innerText.indexOf(guess) !== -1){
-            alreadyUsed.innerText = "You already guessed this character!";
+        //Check if the guess is a whole word
+        if (guess.length > 1){
+            fullWordAttempt(guess);
             return;
         }
-        else{
-            guessed.innerText += guess;
-            alreadyUsed.innerText = "";
-        }
-        //Find index of the character guessed
-        let rightIndex = correctArray.indexOf(guess);
-        if(rightIndex !== -1) {
-            renderGuessed(true, guess);
-            let amountRight = count(wordChosen, guess);
-            for(let i = 0; i < amountRight; i++) {
-                //Updates the right characters guessed
-                rightIndex = correctArray.indexOf(guess);
-                correctProgress = correctProgress.split('');
-                correctProgress[rightIndex] = guess;
-                correctProgress = correctProgress.join('');
-                //Deletes right character to look for remaining characters in the next search
-                delete correctArray[rightIndex];
-            }
-            //Updates the visual for the user
-            renderWord();
-            //Check if the whole word is guessed
-            if(correctProgress === wordChosen) {
-                status.innerText = "You Win!";
-                document.getElementById("input").disabled = true;
-                finishedOverlay.style.display = "flex";
-            }
-        }
-        else{
-            //Display wrong attempt
-            renderGuessed(false, guess);
-            attemptsRemaining--;
-            bodyParts[attemptsRemaining].classList.remove("hidden");
-            if(attemptsRemaining === 2){
-                hint.innerText = hintChosen;
-            }
-            //Check if ran out of tries
-            if(attemptsRemaining === 0){
-                status.innerText = "You Lose :( \nThe Correct Word is: " + wordChosen;
-                document.getElementById("input").disabled = true;
-                finishedOverlay.style.display = "flex";
-            }
-        }
+        //Guess is one letter
+        letterAttempt(guess);
     }
 })
 
+//Event for exiting the rules overlay if the player clicks anywhere on the darkened area
 rulesOverlay.addEventListener("click", (e) => {
     if(e.target.id === "rules-overlay"){
         rulesOverlay.style.display = "none";
     }
 })
 
+//Event for opening up the rules overlay when the player clicks on the informational button
 infobtn.addEventListener("click", () => {
     rulesOverlay.style.display = "flex";
 })
 
+//Event for exiting the rules overlay if the player clicks close
 closerulesbtn.addEventListener("click", () => {
     rulesOverlay.style.display = "none";
 })
 
+//Event for exiting the finished overlay if the player clicks close
 closebtn.addEventListener("click", () => {
     finishedOverlay.style.display = "none";
 })
 
+//Event for exiting the finished overlay if the player clicks anywhere on the darkened area
 finishedOverlay.addEventListener("click", (e) => {
     if(e.target.id === "game-finished"){
         finishedOverlay.style.display = "none";
     }
 })
 
+//Event for exiting the finished overlay if the player clicks restart
 restartbtn.addEventListener("click", () => {
     resetGame();
 })
 
+//Where the game starts
 let themeChosen;
 let wordChosen;
 let hintChosen;
